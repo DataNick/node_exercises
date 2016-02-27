f0dbae1f7fc103f37025711e38aa0a847aa28031
DataNick <DataNick@users.noreply.github.com>
2016-02-27 13:43:09 -0500

    Modified solution

--- baby_steps.js
+++ baby_steps.js
@@ -1,9 +1,15 @@
 // process.argv.forEach(function(val, index, array){
 
 // })
-var result = 0
 
-for (var i = 2; i < process.argv.length; i++)
-  result += Number(process.argv[i])
+// var result = 0
 
-  console.log(result)
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+
+//   console.log(result)
+result = 0
+for (i=2; i < process.argv.length; i++)
+  result += +process.argv[i]// process.argv is an array containing command line arguments.
+// result += Number(process.argv[i]) is another way to solve the problem
+console.log(result)
