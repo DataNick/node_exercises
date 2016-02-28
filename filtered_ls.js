e94e758b4d5817833a2009b81a6c577e5bf33c0a
DataNick <DataNick@users.noreply.github.com>
2016-02-28 14:04:28 -0500

    Fixed syntax

--- filtered_ls.js
+++ filtered_ls.js
@@ -3,9 +3,16 @@ var path = require('path')
 
 var pathName = process.argv[2];
 
-fs.readdir(pathName, function(err, list) {
-	list.forEach(function (file) {
-		if (path.extname(file) === '.' + process.argv[3])
-			console.log(file)
-	})
+// fs.readdir(pathName, function(err, list) {
+// 	list.forEach(function (file) {
+// 		if (path.extname(file) === '.' + process.argv[3])
+// 			console.log(file)
+// 	})
+// });
+
+fs.readdir(pathName, function(err, files){
+  files.forEach(function (file){
+    if (path.extname(file) ==='.' + process.argv[3])
+      console.log(file)
+  })
 });
